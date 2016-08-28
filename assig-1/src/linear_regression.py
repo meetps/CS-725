091f2c6c25e69291e230001fbd18130ef2afce67
meetshah1995 <meetshah1995@gmail.com>
2016-08-28 21:30:32 +0530

    Add preprocessing

--- assig-1/src/linear_regression.py
+++ assig-1/src/linear_regression.py
@@ -4,12 +4,25 @@ import matplotlib.pyplot as plt
 
 from sklearn import datasets, linear_model
 
+def pre_process(X_train, ratio=3.0):
+    for i in range(len(X_train[0])):
+        mean = np.mean(X_train, axis=0) 
+        std =  np.std(X_train, axis=0)
+        print mean, std
+        for j in range(len(X_train)):
+            if X_train[j][i] > mean  + ratio * std:
+                X_train[j][i] = mean  + ratio * std
+            else:
+                pass
+    return X_train
+
+
 def read_training_data(input_path):
     raw_data = pd.read_csv(input_path, header=0)
-    raw_data = raw_data._get_numeric_data()
+    labels = list(pd.read_csv(input_path, nrows=1))
     X = raw_data.as_matrix()[:, 0:-1]
     y = raw_data.as_matrix()[:, -1:]
-    return X, y
+    return X, y, labels
 
 
 def read_test_data(input_path):
@@ -28,15 +41,23 @@ def save_to_csv(preds, fname):
 train_data = "../data/train_data.csv"
 test_data  = "../data/test_data.csv"
 
-X_train, y_train = read_training_data(train_data)
+X_train, y_train, labels = read_training_data(train_data)
 X_test = read_test_data(test_data)
+
+#Preprocessing 
+X_train = pre_process(X_train)
+
+for i in range(1,len(X_train[0])):
+    plt.plot((X_train.T)[i], y_train)
+    plt.title(labels[i])
+    plt.show()
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
-regr.fit(X_train, y_train)
 
-preds = regr.predict(X_test)
-print preds
-save_to_csv(preds.reshape(7643), 'linear_regression_no_pp.csv')
+# regr.fit(X_train, y_train)
+# preds = regr.predict(X_test)
+# print preds
+# save_to_csv(preds.reshape(7643), 'linear_regression_no_pp.csv')
